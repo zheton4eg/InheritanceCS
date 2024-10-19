4d91f8cc245f810aae39c3f224af8da25a211961
zheka <zheka@HOME-PC>
2024-10-19 15:35:06 +0300

    17.10.24 classworkDONE

---
+++ AbstractGeometry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace AbstractGeometry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr hwnd = FindWindow(null,"Microsoft Visual Studio - InheritanceCS");
+            Graphics graphics = Graphics.FromHwnd(hwnd);
+            System.Drawing.Rectangle window_rect = new System.Drawing.Rectangle
+                (
+                Console.WindowLeft, Console.WindowTop,
+                Console.WindowWidth, Console.WindowHeight
+                );
+            PaintEventArgs e = new PaintEventArgs(graphics,window_rect);
+           
+            Rectangle rectangle = new Rectangle(100, 50, 100, 100, 3, System.Drawing.Color.Red);
+            rectangle.Info(e);
+        }
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string className, string windowName);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        
+    }
+}
